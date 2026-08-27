149aee3823ab6dea557f78cd10f2746cca650e03
Taha Rashid <taharashid710192@gmail.com>
2026-08-27 17:47:04 -0400

    Replaced .at(i) with [i] accessor where bounds are not a concern
    In these cases, performance is more of a concern.

--- source/components/ui/DialogueScreen.cpp
+++ source/components/ui/DialogueScreen.cpp
@@ -36,22 +36,22 @@ void DialogueScreen::renderSprites()
 {
     // NOTE: we are currently assuming that the sprite extended palette will be set on VRAM bank I
     vramSetBankI(VRAM_I_LCD);
-    dmaCopy(calendarSprite.at(0).pal, &VRAM_I_EXT_SPR_PALETTE[0][0], calendarSprite.at(0).palLen);
-    dmaCopy(textBox.at(0).pal, &VRAM_I_EXT_SPR_PALETTE[1][0], textBox.at(0).palLen);
-    dmaCopy(nameTag.at(0).pal, &VRAM_I_EXT_SPR_PALETTE[2][0], nameTag.at(0).palLen);
+    dmaCopy(calendarSprite[0].pal, &VRAM_I_EXT_SPR_PALETTE[0][0], calendarSprite[0].palLen);
+    dmaCopy(textBox[0].pal, &VRAM_I_EXT_SPR_PALETTE[1][0], textBox[0].palLen);
+    dmaCopy(nameTag[0].pal, &VRAM_I_EXT_SPR_PALETTE[2][0], nameTag[0].palLen);
     vramSetBankI(VRAM_I_SUB_SPRITE_EXT_PALETTE);
 
     int i = 0;
     oamSet(oam, // sub display (OamState)
            i,   // oam entry to set (id)
-           sprites.at(i).x,
-           sprites.at(i).y,            // position
-           1,                          // priority
-           sprites.at(i).paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
-           sprites.at(i).size,
-           sprites.at(i).format,
-           sprites.at(i).gfx,
-           sprites.at(i).rotationIndex,
+           sprites[i].x,
+           sprites[i].y,            // position
+           1,                       // priority
+           sprites[i].paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
+           sprites[i].size,
+           sprites[i].format,
+           sprites[i].gfx,
+           sprites[i].rotationIndex,
            false, // double the size of rotated sprites
            false, // don't hide the sprite
            true,
@@ -62,14 +62,14 @@ void DialogueScreen::renderSprites()
     i = 1;
     oamSet(oam, // sub display (OamState)
            i,   // oam entry to set (id)
-           sprites.at(i).x,
-           sprites.at(i).y,            // position
-           1,                          // priority
-           sprites.at(i).paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
-           sprites.at(i).size,
-           sprites.at(i).format,
-           sprites.at(i).gfx,
-           sprites.at(i).rotationIndex,
+           sprites[i].x,
+           sprites[i].y,            // position
+           1,                       // priority
+           sprites[i].paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
+           sprites[i].size,
+           sprites[i].format,
+           sprites[i].gfx,
+           sprites[i].rotationIndex,
            false, // double the size of rotated sprites
            false, // don't hide the sprite
            false,
@@ -80,14 +80,14 @@ void DialogueScreen::renderSprites()
     i = 2;
     oamSet(oam, // sub display (OamState)
            i,   // oam entry to set (id)
-           sprites.at(i).x,
-           sprites.at(i).y,            // position
-           1,                          // priority
-           sprites.at(i).paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
-           sprites.at(i).size,
-           sprites.at(i).format,
-           sprites.at(i).gfx,
-           sprites.at(i).rotationIndex,
+           sprites[i].x,
+           sprites[i].y,            // position
+           1,                       // priority
+           sprites[i].paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
+           sprites[i].size,
+           sprites[i].format,
+           sprites[i].gfx,
+           sprites[i].rotationIndex,
            false, // double the size of rotated sprites
            false, // don't hide the sprite
            false,
@@ -98,14 +98,14 @@ void DialogueScreen::renderSprites()
     i = 3;
     oamSet(oam, // sub display (OamState)
            i,   // oam entry to set (id)
-           sprites.at(i).x,
-           sprites.at(i).y,            // position
-           1,                          // priority
-           sprites.at(i).paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
-           sprites.at(i).size,
-           sprites.at(i).format,
-           sprites.at(i).gfx,
-           sprites.at(i).rotationIndex,
+           sprites[i].x,
+           sprites[i].y,            // position
+           1,                       // priority
+           sprites[i].paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
+           sprites[i].size,
+           sprites[i].format,
+           sprites[i].gfx,
+           sprites[i].rotationIndex,
            false, // double the size of rotated sprites
            false, // don't hide the sprite
            false,
@@ -116,14 +116,14 @@ void DialogueScreen::renderSprites()
     i = 4;
     oamSet(oam, // sub display (OamState)
            i,   // oam entry to set (id)
-           sprites.at(i).x,
-           sprites.at(i).y,            // position
-           1,                          // priority
-           sprites.at(i).paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
-           sprites.at(i).size,
-           sprites.at(i).format,
-           sprites.at(i).gfx,
-           sprites.at(i).rotationIndex,
+           sprites[i].x,
+           sprites[i].y,            // position
+           1,                       // priority
+           sprites[i].paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
+           sprites[i].size,
+           sprites[i].format,
+           sprites[i].gfx,
+           sprites[i].rotationIndex,
            false, // double the size of rotated sprites
            false, // don't hide the sprite
            false,
@@ -134,14 +134,14 @@ void DialogueScreen::renderSprites()
     i = 5;
     oamSet(oam, // sub display (OamState)
            i,   // oam entry to set (id)
-           sprites.at(i).x,
-           sprites.at(i).y,            // position
-           1,                          // priority
-           sprites.at(i).paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
-           sprites.at(i).size,
-           sprites.at(i).format,
-           sprites.at(i).gfx,
-           sprites.at(i).rotationIndex,
+           sprites[i].x,
+           sprites[i].y,            // position
+           1,                       // priority
+           sprites[i].paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
+           sprites[i].size,
+           sprites[i].format,
+           sprites[i].gfx,
+           sprites[i].rotationIndex,
            false, // double the size of rotated sprites
            false, // don't hide the sprite
            true,
@@ -152,14 +152,14 @@ void DialogueScreen::renderSprites()
     i = 6;
     oamSet(oam, // sub display (OamState)
            i,   // oam entry to set (id)
-           sprites.at(i).x,
-           sprites.at(i).y,            // position
-           1,                          // priority
-           sprites.at(i).paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
-           sprites.at(i).size,
-           sprites.at(i).format,
-           sprites.at(i).gfx,
-           sprites.at(i).rotationIndex,
+           sprites[i].x,
+           sprites[i].y,            // position
+           1,                       // priority
+           sprites[i].paletteAlpha, // palette for 16 color sprite or alpha for bmp sprite
+           sprites[i].size,
+           sprites[i].format,
+           sprites[i].gfx,
+           sprites[i].rotationIndex,
            false, // double the size of rotated sprites
            false, // don't hide the sprite
            false,
@@ -170,7 +170,7 @@ void DialogueScreen::renderSprites()
     // rotate sprite
     // oamRotateScale(
     //     oam,
-    //     sprites.at(i).rotationIndex,
+    //     sprites[i].rotationIndex,
     //     degreesToAngle(0),
     //     intToFixed(1, 8),
     //     intToFixed(1, 8)
@@ -183,13 +183,13 @@ void DialogueScreen::renderSprites()
     //     oamSet(
     //         oam,                    // sub display (OamState)
     //         7 + i,                      // oam entry to set (id)
-    //         sprites.at(7).x + (13 * i), sprites.at(7).y, // position
+    //         sprites[7].x + (13 * i), sprites[7].y, // position
     //         1,                          // priority
-    //         sprites.at(7).paletteAlpha,    // palette for 16 color sprite or alpha for bmp sprite
-    //         sprites.at(7).size,
-    //         sprites.at(7).format,
-    //         sprites.at(7).gfx,
-    //         sprites.at(7).rotationIndex,
+    //         sprites[7].paletteAlpha,    // palette for 16 color sprite or alpha for bmp sprite
+    //         sprites[7].size,
+    //         sprites[7].format,
+    //         sprites[7].gfx,
+    //         sprites[7].rotationIndex,
     //         true,         // double the size of rotated sprites
     //         false,        // don't hide the sprite
     //         false, false, // vflip, hflip
@@ -217,51 +217,51 @@ void DialogueScreen::load()
 
     // setup sprites
     // calendar
-    sprites.at(0) = {0, SpriteSize_64x64, SpriteColorFormat_256Color, -1, 0, 192, 0};
-    sprites.at(1) = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 0, 128, 0};
+    sprites[0] = {0, SpriteSize_64x64, SpriteColorFormat_256Color, -1, 0, 192, 0};
+    sprites[1] = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 0, 128, 0};
     // text box
-    sprites.at(2) = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 1, 0, 128};
-    sprites.at(3) = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 1, 64, 128};
-    sprites.at(4) = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 1, 128, 128};
-    sprites.at(5) = {0, SpriteSize_64x64, SpriteColorFormat_256Color, -1, 1, 192, 128};
+    sprites[2] = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 1, 0, 128};
+    sprites[3] = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 1, 64, 128};
+    sprites[4] = {0, SpriteSize_64x64, SpriteColorFormat_256Color, 0, 1, 128, 128};
+    sprites[5] = {0, SpriteSize_64x64, SpriteColorFormat_256Color, -1, 1, 192, 128};
     // name tag
-    sprites.at(6) = {0, SpriteSize_64x32, SpriteColorFormat_256Color, 0, 2, 20, 112};
+    sprites[6] = {0, SpriteSize_64x32, SpriteColorFormat_256Color, 0, 2, 20, 112};
 
     // allocating space for sprite graphics
     // calendar
-    sprites.at(0).gfx = oamAllocateGfx(oam, sprites.at(0).size, sprites.at(0).format);
-    sprites.at(1).gfx = oamAllocateGfx(oam, sprites.at(1).size, sprites.at(1).format);
+    sprites[0].gfx = oamAllocateGfx(oam, sprites[0].size, sprites[0].format);
+    sprites[1].gfx = oamAllocateGfx(oam, sprites[1].size, sprites[1].format);
     // text box
-    sprites.at(2).gfx = oamAllocateGfx(oam, sprites.at(2).size, sprites.at(2).format);
-    sprites.at(3).gfx = oamAllocateGfx(oam, sprites.at(3).size, sprites.at(3).format);
-    sprites.at(4).gfx = oamAllocateGfx(oam, sprites.at(4).size, sprites.at(4).format);
-    sprites.at(5).gfx = oamAllocateGfx(oam, sprites.at(5).size, sprites.at(5).format);
+    sprites[2].gfx = oamAllocateGfx(oam, sprites[2].size, sprites[2].format);
+    sprites[3].gfx = oamAllocateGfx(oam, sprites[3].size, sprites[3].format);
+    sprites[4].gfx = oamAllocateGfx(oam, sprites[4].size, sprites[4].format);
+    sprites[5].gfx = oamAllocateGfx(oam, sprites[5].size, sprites[5].format);
     // name tag
-    sprites.at(6).gfx = oamAllocateGfx(oam, sprites.at(6).size, sprites.at(6).format);
+    sprites[6].gfx = oamAllocateGfx(oam, sprites[6].size, sprites[6].format);
 
     // get sprites
     // calendar
-    calendarSprite.at(0) = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::CALENDAR);
-    calendarSprite.at(1) = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::CALENDAR);
+    calendarSprite[0] = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::CALENDAR);
+    calendarSprite[1] = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::CALENDAR);
     // text box
-    textBox.at(0) = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_CORNER);
-    textBox.at(1) = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_MIDDLE);
-    textBox.at(2) = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_MIDDLE);
-    textBox.at(3) = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_CORNER);
+    textBox[0] = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_CORNER);
+    textBox[1] = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_MIDDLE);
+    textBox[2] = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_MIDDLE);
+    textBox[3] = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::TEXT_CORNER);
     // name tag
-    nameTag.at(0) = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::NAME_TAG);
+    nameTag[0] = graphics->loadSpriteGraphic(spritePath, SpriteType::DIALOGUE, DialogueSprite::NAME_TAG);
 
     // copy sprites into memory
     // calendar
-    dmaCopy(calendarSprite.at(0).tiles, sprites.at(0).gfx, calendarSprite.at(0).tilesLen);
-    dmaCopy(calendarSprite.at(1).tiles, sprites.at(1).gfx, calendarSprite.at(1).tilesLen);
+    dmaCopy(calendarSprite[0].tiles, sprites[0].gfx, calendarSprite[0].tilesLen);
+    dmaCopy(calendarSprite[1].tiles, sprites[1].gfx, calendarSprite[1].tilesLen);
     // text box
-    dmaCopy(textBox.at(0).tiles, sprites.at(2).gfx, textBox.at(0).tilesLen);
-    dmaCopy(textBox.at(1).tiles, sprites.at(3).gfx, textBox.at(1).tilesLen);
-    dmaCopy(textBox.at(2).tiles, sprites.at(4).gfx, textBox.at(2).tilesLen);
-    dmaCopy(textBox.at(3).tiles, sprites.at(5).gfx, textBox.at(3).tilesLen);
+    dmaCopy(textBox[0].tiles, sprites[2].gfx, textBox[0].tilesLen);
+    dmaCopy(textBox[1].tiles, sprites[3].gfx, textBox[1].tilesLen);
+    dmaCopy(textBox[2].tiles, sprites[4].gfx, textBox[2].tilesLen);
+    dmaCopy(textBox[3].tiles, sprites[5].gfx, textBox[3].tilesLen);
     // name tag
-    dmaCopy(nameTag.at(0).tiles, sprites.at(6).gfx, nameTag.at(0).tilesLen);
+    dmaCopy(nameTag[0].tiles, sprites[6].gfx, nameTag[0].tilesLen);
 };
 
 void DialogueScreen::unload()
