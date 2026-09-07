5412c3a89a2e2161c06d8f474f393ec6f9ecb701
Nolan Kolb <nolankolb818@gmail.com>
2026-09-07 20:20:01 +0200

    Replace floats with fixed point math  (#547)
    * Add math functions
    
    Add fixed point math functions
    
    * New math funcs
    
    New math funcs for fixed point math, also corrected some to use radians isntead of angle
    
    * Added ae prefixes
    
    ae namespace for fixed types
    
    * Replace various battle types
    
    Floats to fixed point
    
    * Replaced remaining battlecalcs
    
    all battllecalcs now use  ae::q20_12_t instead of floats
    
    * Implemented fixed point math in camera system
    
    All floats have been replaced in the camera system. added some new math manager funcs
    
    * Replace floats  movementComponent
    
    * Replaced floats in Music  / animation Controller
    
    Also added a new  secondsToSample func to the math manager to avoid overflows while doing some calcs
    
    * Replaced floats in video controller / enviorment view
    
    * Converted floats to fixed pooint in enviorment hpp / cpp
    
    * Adjust views to use fixed point math
    
    * Finally compiles
    
    * Update aegis_engine
    
    * Python files
    
    * Fixed emulated div
    
    In Movementcomponent
    
    * Fix Player Sprtie Orinetation
    
    some values were flipped
    
    * Used hardware accelerated div in Battlcalcs
    
    * Used simpler formulas
    
    * Fixed audio looping
    
    Previously calculated audio start point wrong
    
    * Unchange videos
    
    For some reason showed as changed (windows?)
    
    * Fix merge
    
    Some changes werent clean, mostly because types were refactored into diffrent files which needed to be changed now
    
    * Fix Atan2 parameter passing order
    
    Had x/ z swapped
    
    * Fix MathManager / aegis includes
    
    Add includes for new ndsTypes.hpp, depreciate relative imports for MathManager.hpp
    
    * Documented BattleCalcs
    
    Documented BattleCalcs, removed stray documentation in cpp file, simplified getAtk()
    
    * Made hitrate to hit check
    
    In battleCalcs, since its only used as a bool anyways
    
    * Make hit caclulation use fixed point math
    
    Dosen't need to be u32, dosnet get applied anywhere directly
    
    * Move back battlecalcs functions
    
    Do actually need to be public
    
    * Clean up mathmanger calls
    
    When appropriate  now just get saved on the class
    
    * Update aegis engine  submodule
    
    Needed for the fixed point math changes to the aegis_engine
    
    ---------
    
    Co-authored-by: n.kolb <n.kolb@rafisa.ch>

--- source/components/TextComponent.cpp
+++ source/components/TextComponent.cpp
@@ -1,8 +1,9 @@
 #include "TextComponent.hpp"
-
+#include <aegis/ndsTypes.hpp>
+#include <aegis/types.hpp>
 #include <algorithm>
 
-void TextComponent::Update(ae::fixed_t)
+void TextComponent::Update(ae::q20_12_t)
 {
     if (appearingText != nullptr)
     {
